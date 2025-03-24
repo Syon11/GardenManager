a3ec60b01bf2031f8dca504932d3e1f4a7edfbb4
Phil P. <philp.lafond@gmail.com>
2025-03-24 12:55:01 -0400

    changed Competence constructors and model

--- GardenManager/Model/Competence.cs
+++ GardenManager/Model/Competence.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using GardenManager.Enums;
 
 namespace GardenManager.Model;
 
@@ -8,43 +9,58 @@ public class Competence
     public string Name { get; set; }
     public string Description { get; set; }
     public int Tier { get; set; }
+    public Trunk Trunk { get; set; }
+    public Specialisation Specialisation { get; set; }
     public List<Variant> Variants { get; set; }
     public string? VocalComponent { get; set; }
     public long? PrerequisiteId { get; set; }
     public List<Character> Characters { get; set; }
 
     public Competence(){}
-    public Competence(long id, string name, string description, List<Variant> variants)
+    public Competence(long id, string name, int tier, Trunk trunk, Specialisation spec, string description, List<Variant> variants)
     {
         Id = id;
         Name = name;
+        Tier = tier;
+        Trunk = trunk;
+        Specialisation = spec;
         Description = description;
         Variants = variants;
+        
     }
 
-    public Competence(long id, string name, string description, List<Variant> variants, string vocalComponent)
+    public Competence(long id, string name, int tier, Trunk trunk, Specialisation spec, string description, List<Variant> variants, string vocalComponent)
     {
         Id = id;
         Name = name;
+        Tier = tier;
+        Trunk = trunk;
+        Specialisation = spec;
         Description = description;
         Variants = variants;
         VocalComponent = vocalComponent;
     }
 
-    public Competence(long id, string name, string description, List<Variant> variants, long prerequisiteId)
+    public Competence(long id, string name, int tier, Trunk trunk, Specialisation spec, string description, List<Variant> variants, long prerequisiteId)
     {
         Id = id;
         Name = name;
+        Tier = tier;
+        Trunk = trunk;
+        Specialisation = spec;
         Description = description;
         Variants = variants;
         PrerequisiteId = prerequisiteId;
     }
 
-    public Competence(long id, string name, string description, List<Variant> variants, string vocalComponent,
+    public Competence(long id, string name, int tier, Trunk trunk, Specialisation spec, string description, List<Variant> variants, string vocalComponent,
         long prerequisiteId)
     {
         Id = id;
         Name = name;
+        Tier = tier;
+        Trunk = trunk;
+        Specialisation = spec;
         Description = description;
         Variants = variants;
         VocalComponent = vocalComponent;
